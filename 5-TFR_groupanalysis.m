daaafad20461d106f6502b98cecbf0eab8b23ab7
JoWld <54455187+JoWld@users.noreply.github.com>
2024-06-19 12:57:10 +0200

    Update 5-TFR_groupanalysis.m

--- 5-TFR_groupanalysis.m
+++ 5-TFR_groupanalysis.m
@@ -5,32 +5,31 @@
 
 cd '/Users/huser/Documents/ot/meg_data/';
 
-%subs = dir('/Users/huser/Documents/ot/meg_data/');                                %Find subjects in folder
+%subs = dir('/Users/huser/Documents/ot/meg_data/');      %Find subjects in folder
 %subs = {subs([subs.isdir]).name};                       %Make list with all subjects that have a folder
+
+% define OT and HC groups, 2 = foot (but subs and subs2 are identical in final analysis)
 OT_subs = intersect(OT,subs);
 ctrl_subs = intersect(ctrl,subs);
+ctrl_subs2 = intersect(ctrl,subs2);
+OT_subs2 = intersect(OT,subs2);
 
-%% Define bands
-theta_band      = [4 7];
+
+%% Define band
 betamu_band     = [8 30];
-lowgamma_band   = [35 40];
-%highgamma_band  = [55 100];
-%gamma_band      = [35 100];
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Log/transform and extract single channel for Beta & Mu analysis
 
-% Find peak
-time = [0.05 .110];
+time = [0.05 .110]; %time window for peak detection
 
-data_OT1 = cell(1,length(OT_subs));
+data_OT1 = cell(1,length(OT_subs2));
 data_OT2 = cell(1,length(OT_subs));
-data_ctrl1 = cell(1,length(ctrl_subs));
+data_ctrl1 = cell(1,length(ctrl_subs2));
 data_ctrl2 = cell(1,length(ctrl_subs));
 
-OT_beta1 = cell(1,length(OT_subs));
+OT_beta1 = cell(1,length(OT_subs2));
 OT_beta2 = cell(1,length(OT_subs));
-ctrl_beta1 = cell(1,length(ctrl_subs));
+ctrl_beta1 = cell(1,length(ctrl_subs2));
 ctrl_beta2 = cell(1,length(ctrl_subs));
 
 data_OT1log = OT_beta1;
@@ -43,22 +42,20 @@ OT_beta2bs = OT_beta2;
 ctrl_beta1bs = ctrl_beta1;
 ctrl_beta2bs = ctrl_beta2;
 
-channam_OT = cell(1,length(OT_subs));
-channam_ctrl = cell(1,length(ctrl_subs));
+channam_OT = cell(1,length(OT_subs2));
+channam_ctrl = cell(1,length(ctrl_subs2));
+channam_OT2 = cell(1,length(OT_subs));
+channam_ctrl2 = cell(1,length(ctrl_subs));
 
-
-for ii = 1:length(OT_subs)
-    subID = OT_subs{ii};
+for ii = 1:length(OT_subs2)
+    subID = OT_subs2{ii};
     disp(subID);
     sub_dir = ['/Users/huser/Documents/ot/meg_data/',subID];
-    load([sub_dir,'/1-evoked.mat']);
+    load([sub_dir,'/foot-evoked.mat']);
     timelocked = ft_combineplanar([],timelocked);
-    
     [channam_OT{ii},~,~] = find_ERF_peak(timelocked,time);
-    
-    % Foot
-    load([sub_dir,'/1-tfr.mat']);
-    
+   
+    load([sub_dir,'/foot-tfr.mat']);
     data_OT1{ii} = tfr;
     
     cfg           = [];
@@ -75,7 +72,6 @@ for ii = 1:length(OT_subs)
     OT_beta1{ii} = ft_selectdata(cfg, data_OT1log{ii});
     
     OT_beta1{ii}.label = {'peak_channel'};
-    
     cfg = [];
     cfg.baseline        = [-inf -0.2];
     cfg.baselinetype    = 'absolute';
@@ -84,13 +80,24 @@ for ii = 1:length(OT_subs)
     OT_beta1bs{ii} = ft_freqbaseline(cfg, OT_beta1{ii});
         
     clear tfr
-    
+end
+
+for ii = 1:length(OT_subs)
+    subID = OT_subs{ii};
+    disp(subID);
+    sub_dir = ['/Users/huser/Documents/ot/meg_data/',subID]
     % Hand
-    load([sub_dir,'/evoked.mat']);
+    load([sub_dir,'/hand-evoked.mat']);
     timelocked = ft_combineplanar([],timelocked);
-    [channam_ctrl{ii},~,~] = find_ERF_peak(timelocked,time);
-    
-    load([sub_dir,'/tfr.mat']);
+    [channam_OT2{ii},~,~] = find_ERF_peak(timelocked,time);
+
+    % visualise the gradiometer data for pre-check
+%     cfg        = [];
+%     cfg.layout = 'neuromag306cmb_helmet.mat';
+%     figure; ft_multiplotER(cfg, timelocked);
+%    
+% 
+    load([sub_dir,'/hand-tfr.mat']);
     
     data_OT2{ii} = tfr;
       
@@ -101,7 +108,7 @@ for ii = 1:length(OT_subs)
     data_OT2log{ii} = ft_math(cfg, data_OT2{ii});
        
     cfg = [];
-    cfg.channel = channam_OT{ii};
+    cfg.channel = channam_OT2{ii};
     cfg.frequency = betamu_band;
     
     OT_beta2{ii} = ft_selectdata(cfg, data_OT2log{ii});
@@ -118,16 +125,16 @@ for ii = 1:length(OT_subs)
     clear tfr
 end
 
-for ii = 1:length(ctrl_subs)
-    subID = ctrl_subs{ii};
+for ii = 1:length(ctrl_subs2)
+    subID = ctrl_subs2{ii};
     disp(subID);
     sub_dir = ['/Users/huser/Documents/ot/meg_data/',subID];
-    load([sub_dir,'/1-evoked.mat']);
+    load([sub_dir,'/foot-evoked.mat']);
     timelocked = ft_combineplanar([],timelocked);
     [channam_ctrl{ii},~,~] = find_ERF_peak(timelocked,time);
     
     % Foot
-    load([sub_dir,'/1-tfr.mat']);
+    load([sub_dir,'/foot-tfr.mat']);
       
     data_ctrl1{ii} = tfr;
 
@@ -152,13 +159,18 @@ for ii = 1:length(ctrl_subs)
         
     ctrl_beta1bs{ii} = ft_freqbaseline(cfg, ctrl_beta1{ii});
     clear tfr
-    
-    % Hand
-    load([sub_dir,'/evoked.mat']);
-    timelocked = ft_combineplanar([],timelocked);
-    [channam_ctrl{ii},~,~] = find_ERF_peak(timelocked,time);
+end
+for ii = 1:length(ctrl_subs)
+    subID = ctrl_subs{ii};
+    disp(subID);
+    sub_dir = ['/Users/huser/Documents/ot/meg_data/',subID];
 
-    load([sub_dir,'/tfr.mat']);
+    % Hand
+    load([sub_dir,'/hand-evoked.mat']);
+    timelocked = ft_combineplanar([],timelocked);
+    [channam_ctrl2{ii},~,~] = find_ERF_peak(timelocked,time);
+
+    load([sub_dir,'/hand-tfr.mat']);
     
     data_ctrl2{ii} = tfr;
 
@@ -169,11 +181,10 @@ for ii = 1:length(ctrl_subs)
     data_ctrl2log{ii} = ft_math(cfg, data_ctrl2{ii});
     
     cfg = [];
-    cfg.channel = channam_ctrl{ii};
+    cfg.channel = channam_ctrl2{ii};
     cfg.frequency = betamu_band;
     
     ctrl_beta2{ii} = ft_selectdata(cfg, data_ctrl2log{ii});
-    
     ctrl_beta2{ii}.label = {'peak_channel'};
     
     cfg = [];
@@ -189,35 +200,35 @@ save(['/Users/huser/Documents/ot/','/betaChan1log.mat'],'ctrl_beta1','OT_beta1',
 disp('saved 1 of 5');
 save(['/Users/huser/Documents/ot/betaChan1bs.mat'],'ctrl_beta1bs','OT_beta1bs','ctrl_beta2bs','OT_beta2bs','-v7.3');
 disp('saved 2 of 5');
-save(['/Users/huser/Documents/ot/all_TFRlog.mat'],'data_ctrl1log','data_ctrl2log','data_OT1log','data_OT2log','-v7.3');
-disp('saved 3 of 5');
-save(['/Users/huser/Documents/ot/all_TFR.mat'],'data_ctrl1','data_ctrl2','data_OT1','data_OT2','-v7.3');
-disp('saved 4 of 5');
-save(['/Users/huser/Documents/ot/peak_chans.mat'],'channam_ctrl','channam_OT','-v7.3');
-disp('done')
+% save(['/Users/huser/Documents/ot/all_TFRlog.mat'],'data_ctrl1log','data_ctrl2log','data_OT1log','data_OT2log','-v7.3');
+% disp('saved 3 of 5');
+% save(['/Users/huser/Documents/ot/all_TFR.mat'],'data_ctrl1','data_ctrl2','data_OT1','data_OT2','-v7.3');
+% disp('saved 4 of 5');
+% save(['/Users/huser/Documents/ot/peak_chans.mat'],'channam_ctrl','channam_ctrl2','channam_OT','channam_OT2','-v7.3');
+% disp('done')
 
-cd('/Users/huser/Documents/ot/');
-clear data* ctrl_beta* OT_beta*
+%cd('/Users/huser/Documents/ot/');
+%clear data* ctrl_beta* OT_beta*
 
-%% Low-freq: Flip if necessary and baseline correct
-load(['/Users/huser/Documents/ot/all_TFRlog.mat']);
-disp('done')
-
-%% Baseline correct
-data_OT1bs = cell(1,length(OT_subs));
-data_OT2bs = cell(1,length(OT_subs));
-data_ctrl1bs = cell(1,length(ctrl_subs));
-data_ctrl2bs = cell(1,length(ctrl_subs));
-
-%Baseline options
-cfg = [];
-cfg.baseline        = [-inf -0.2];
-cfg.baselinetype    = 'absolute';
-cfg.parameter       = 'powspctrm';
-
-for ii = 1:length(OT_subs)
-    subID = OT_subs{ii};
-    disp(subID);
+% %% Low-freq: Flip if necessary and baseline correct
+% load(['/Users/huser/Documents/ot/all_TFRlog.mat']);
+% disp('done')
+% 
+% %% Baseline correct
+% data_OT1bs = cell(1,length(OT_subs2));
+% data_OT2bs = cell(1,length(OT_subs));
+% data_ctrl1bs = cell(1,length(ctrl_subs2));
+% data_ctrl2bs = cell(1,length(ctrl_subs));
+% 
+% %Baseline options
+% cfg = [];
+% cfg.baseline        = [-inf -0.2];
+% cfg.baselinetype    = 'absolute';
+% cfg.parameter       = 'powspctrm';
+% 
+% for ii = 1:length(OT_subs2)
+%     subID = OT_subs2{ii};
+%     disp(subID);
 %     sub_dir = [dirs.megDir,'/',subID];
     
     %Session 1+2
@@ -226,40 +237,68 @@ for ii = 1:length(OT_subs)
 %         data_OT1log{ii} = flip_sens_neuromag(data_OT1log{ii});
 %         data_OT2log{ii} = flip_sens_neuromag(data_OT2log{ii});
 %     end    
-    data_OT1bs{ii} = ft_freqbaseline(cfg,data_OT1log{ii});
-    data_OT2bs{ii} = ft_freqbaseline(cfg,data_OT2log{ii});
-    
-end
-
-for ii = 1:length(ctrl_subs)
-    subID = ctrl_subs{ii};
-    disp(subID);
-    
-    %Session 1+2
-%     if any(strcmp(lh_subs,subID))
-%         disp('Flip')
-%         data_ctrl1log{ii} = flip_sens_neuromag(data_crtl1log{ii});
-%         data_ctrl2log{ii} = flip_sens_neuromag(data_ctrl2log{ii});
-%     end    
-    data_ctrl1bs{ii} = ft_freqbaseline(cfg,data_ctrl1log{ii});
-    data_ctrl2bs{ii} = ft_freqbaseline(cfg,data_ctrl2log{ii});
-    
-end
-
-save(['/Users/huser/Documents/ot/all_TFRbs.mat'],'data_ctrl1bs','data_ctrl2bs','data_OT1bs','data_OT2bs','-v7.3');
-disp('done')
-
-%% Average
-% cfg = [];
-% cfg.keepindividual = 'yes';
+%     data_OT1bs{ii} = ft_freqbaseline(cfg,data_OT1log{ii});
+%     
+% end
 % 
-% avgTFR.OT1 = ft_freqgrandaverage(cfg, data_OT1{:});
-% avgTFR.OT2 = ft_freqgrandaverage(cfg, data_OT2{:});
-% avgTFR.ctrl1 = ft_freqgrandaverage(cfg, data_ctrl1{:});
-% avgTFR.ctrl2 = ft_freqgrandaverage(cfg, data_ctrl2{:});
+% for ii = 1:length(OT_subs)
+%     subID = OT_subs{ii};
+%     disp(subID);
+% %     sub_dir = [dirs.megDir,'/',subID];
+%     
+%     %Session 1+2
+% %     if any(strcmp(lh_subs,subID))
+% %         disp('Flip')
+% %         data_OT1log{ii} = flip_sens_neuromag(data_OT1log{ii});
+% %         data_OT2log{ii} = flip_sens_neuromag(data_OT2log{ii});
+% %     end    
+%     data_OT2bs{ii} = ft_freqbaseline(cfg,data_OT2log{ii});
+%     
+% end
 % 
-% % Save data
-% save([dirs.output,'/avgTFR.mat'],'avgTFR','-v7.3');
+% 
+% for ii = 1:length(ctrl_subs2)
+%     subID = ctrl_subs2{ii};
+%     disp(subID);
+%     
+%     %Session 1+2
+% %     if any(strcmp(lh_subs,subID))
+% %         disp('Flip')
+% %         data_ctrl1log{ii} = flip_sens_neuromag(data_crtl1log{ii});
+% %         data_ctrl2log{ii} = flip_sens_neuromag(data_ctrl2log{ii});
+% %     end    
+%     data_ctrl1bs{ii} = ft_freqbaseline(cfg,data_ctrl1log{ii});
+% 
+%     
+% end
+% 
+% for ii = 1:length(ctrl_subs)
+%     subID = ctrl_subs{ii};
+%     disp(subID);
+%     
+%     %Session 1+2
+% %     if any(strcmp(lh_subs,subID))
+% %         disp('Flip')
+% %         data_ctrl1log{ii} = flip_sens_neuromag(data_crtl1log{ii});
+% %         data_ctrl2log{ii} = flip_sens_neuromag(data_ctrl2log{ii});
+% %     end    
+%     data_ctrl2bs{ii} = ft_freqbaseline(cfg,data_ctrl2log{ii});
+%     
+% end
+% save(['/Users/huser/Documents/ot/all_TFRbs.mat'],'data_ctrl1bs','data_ctrl2bs','data_OT1bs','data_OT2bs','-v7.3');
 % disp('done')
+% 
+% %% Average
+% % cfg = [];
+% % cfg.keepindividual = 'yes';
+% % 
+% % avgTFR.OT1 = ft_freqgrandaverage(cfg, data_OT1{:});
+% % avgTFR.OT2 = ft_freqgrandaverage(cfg, data_OT2{:});
+% % avgTFR.ctrl1 = ft_freqgrandaverage(cfg, data_ctrl1{:});
+% % avgTFR.ctrl2 = ft_freqgrandaverage(cfg, data_ctrl2{:});
+% % 
+% % % Save data
+% % save([dirs.output,'/avgTFR.mat'],'avgTFR','-v7.3');
+% % disp('done')
 
-exit
+%exit
