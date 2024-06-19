b1a1675171df5d6741fa2302f185549ddf9f98db
JoWld <54455187+JoWld@users.noreply.github.com>
2024-06-19 13:07:55 +0200

    Update 8-plots.m

--- 8-plots.m
+++ 8-plots.m
@@ -1,109 +1,14 @@
-%%%%%%%%%%%% Plots for OT proprioception project  %%%%%%%%%%%
+%%%%%%%%%%%%% Make plots for publication for OT-project %%%%%%%%%%%
+% create plot of averaged hand and foot ERS per group, then single plot for each participant
 
-OT_subs = intersect(subs, OT);
-ctrl_subs = intersect(subs, ctrl);
 
-%% ################ TFR FIGURE ##################
-%% load data
-load(['/Users/huser/Documents/ot/betaChan1bs.mat']);
-% load(['/Users/huser/Documents/ot/acc_ERP2.mat']);
-load(['/Users/huser/Documents/ot/emg_raw.mat']);
-load(['/Users/huser/Documents/ot/beta_stats.mat'])
+cd '/Users/huser/Documents/ot/meg_data';
+OT_subs = intersect(OT,subs);
+ctrl_subs = intersect(ctrl,subs);
+OT_subs2 = intersect(OT,subs2);
+ctrl_subs2 = intersect(ctrl,subs2);
 
-disp('data loaded');
-
-%% Grand average
-avgTFR.OT_beta1 = ft_freqgrandaverage([],OT_beta1bs{:});
-avgTFR.OT_beta2 = ft_freqgrandaverage([],OT_beta2bs{:});
-avgTFR.ctrl_beta1 = ft_freqgrandaverage([],ctrl_beta1bs{:});
-avgTFR.ctrl_beta2 = ft_freqgrandaverage([],ctrl_beta2bs{:});
-
-% acc_OT1e = acc_OT1(~cellfun('isempty',acc_OT1)); %Remove empty field
-% % accPow_OT2e = accPow_OT2(~cellfun('isempty',acc_OT1)); %Remove empty field
-% accAvg.OT_beta1 = ft_timelockgrandaverage([],acc_OT1e{:});
-% accAvg.OT_beta2 = ft_timelockgrandaverage([],acc_OT2{:});
-% accAvg.ctrl_beta1 = ft_timelockgrandaverage([],acc_ctrl1{:});
-% accAvg.ctrl_beta2 = ft_timelockgrandaverage([],acc_ctrl2{:});
-% 
-% accAvg.OT_beta1.avg = zscore(accAvg.OT_beta1.avg);
-% accAvg.OT_beta2.avg = zscore(accAvg.OT_beta2.avg);
-% accAvg.ctrl_beta1.avg = zscore(accAvg.ctrl_beta1.avg);
-% accAvg.ctrl_beta2.avg = zscore(accAvg.ctrl_beta2.avg);
-
-emg_avg.OT_beta1 = ft_timelockgrandaverage([],OT_emg1{:});
-emg_avg.OT_beta2 = ft_timelockgrandaverage([],OT_emg2{:});
-emg_avg.ctrl_beta1 = ft_timelockgrandaverage([],ctrl_emg1{:});
-emg_avg.ctrl_beta2 = ft_timelockgrandaverage([],ctrl_emg2{:});
-
-conds = fields(avgTFR);
-
-for ii = 1:length(conds)
-    emg_avg.(conds{ii}).avg = emg_avg.(conds{ii}).avg*1000; % Change unit mV
-end
-
-%% Make seperate plots per group x session
-conds = fieldnames(avgTFR);
-
-cfg = [];
-cfg.zlim        = [-0.45 0.45];
-cfg.xlim        = [-.5 2.5];
-cfg.colorbar    = 'no';
-% cfg.colormap    = 'jet';
-cfg.interactive = 'no';
-
-cfgTFR = cfg;
-% cfgACC = cfg;
-% cfgACC.ylim     = [-1 25];
-% cfgACC.fontsize = 5;
-% cfgACC.graphcolor = [0 0 0];
-cfgEMG = cfg;
-cfgEMG.ylim     = [-1 1];
-cfgEMG.fontsize = 5;
-cfgEMG.graphcolor = [0 0 0];
-
-titles = {
-    'OT foot'
-    'OT finger'
-    'Healthy controls foot'
-    'Healthy controls finger'};
-
-for ii = 1:length(conds)
-    fig = figure;
-    subplot(4,2,ii); ft_singleplotTFR(cfgTFR, avgTFR.(conds{ii}));
-    ax1 = gca();
-    ft_plot_line([0 0],ax1.YLim, 'color','k','linestyle','--');
-    box on
-    title(titles{ii},'fontsize',12);
-    set(gca, 'LineWidth', 2);
-    y1 = ylabel('Freq. (Hz)'); xlabel('Time (s)'); 
-    colormap(rbmap) 
-
-%     subplot(20,1,16:17); ft_singleplotER(cfgACC, accAvg.(conds{ii}));
-%     y2 = ylabel({'Acceleration';'(z-score)'},'fontsize',6);
-%     set(gca,'xtick',[],'xticklabel','','XColor','none','LineWidth', 1 )
-%     set(y2, 'HorizontalAlignment','center','VerticalAlignment','bottom')
-%     title(' ');
-    
-    subplot(4,2,ii*2); ft_singleplotER(cfgEMG, emg_avg.(conds{ii}));
-    y2 = ylabel({'EMG';'(mV)'},'fontsize',6); 
-    set(gca,'LineWidth', 1)
-    set(y2,'HorizontalAlignment','center','VerticalAlignment','bottom'); %    y2.Position)
-    title(' ');
-    
-%     set(findobj(gcf,'type','axes'), 'LineWidth', 1);
-    set(fig,'PaperPosition', [0 0 4 2], 'color','w');
-
-%     savefig(['TFR_',conds{ii},'.fig']);
-%     print('-dpng','-r500',['TFR_',conds{ii},'.png'])
-%     print('-painters','-dpdf',['TFR_',conds{ii},'.pdf'])
-
-%    export_fig(['TFR_',conds{ii},'exp2.png'], '-r500', '-p0.05', '-CMYK')
-%    export_fig(['TFR_',conds{ii},'exp2.pdf'], '-r500', '-p0.05', '-CMYK', '-pdf')
-
-end
-disp('DONE');
-
-%% ####### Extract beta traces and plot in single figure ############
+%% Extract beta traces and plot in single figure
 timeDim = avgTFR.OT_beta1.time;
 
 cfg = [];
@@ -125,7 +30,7 @@ BtraceCtrl2 = squeeze(BdataCtrl2.powspctrm);
 BtraceOT1sub = cell(1,length(OT_beta1bs));
 BtraceOT2sub = cell(1,length(OT_beta2bs));
 BtraceCtrl1sub = cell(1,length(ctrl_beta1bs));
-BtraceCtrl2sub = cell(1,length(ctrl_beta1bs));
+BtraceCtrl2sub = cell(1,length(ctrl_beta2bs));
 
 for ss = 1:length(OT_beta1bs)
     temp = ft_selectdata(cfg,OT_beta1bs{ss});
@@ -141,34 +46,34 @@ for ss = 1:length(ctrl_beta1bs)
 end
     
 % Get cluster
-stat_beta2.sigposmask = (stat_beta2.posclusterslabelmat==1) & stat_beta2.mask;
-stat_beta2.signegmask = (stat_beta2.negclusterslabelmat==1) & stat_beta2.mask;
-
-% extract start and end of significant negative cluster
-clustStart = stat_beta2.time(find(sum(squeeze(stat_beta2.signegmask)), 1,'first'));
-clustEnd = stat_beta2.time(find(sum(squeeze(stat_beta2.signegmask)), 1,'last'));
+ stat_beta1.sigposmask = (stat_beta1.posclusterslabelmat==1) & stat_beta1.mask;
+ stat_beta1.signegmask = (stat_beta1.negclusterslabelmat==1) & stat_beta1.mask;
+% 
+% % extract start and end of significant negative cluster
+ clustStart = stat_beta1.time(find(sum(squeeze(stat_beta1.mask)), 1,'first'));
+ clustEnd = stat_beta1.time(find(sum(squeeze(stat_beta1.mask)), 1,'last'));
 
 % make plot
 fig = figure('rend','painters','pos',[10 10 1000 800]); hold on
 set(fig,'PaperPosition', [0 0 4 2], 'color','w');
 
-patch([clustStart clustEnd clustEnd clustStart],[-0.6 -0.6 0.4 0.4],[.1,.1,.1],'FaceAlpha',0.15,'EdgeColor','none')
+% patch([clustStart clustEnd clustEnd clustStart],[-0.6 -0.6 0.4 0.4],[.1,.1,.1],'FaceAlpha',0.15,'EdgeColor','none')
 for ss = 1:length(BtraceCtrl1sub)
-    ps1 = plot(timeDim,BtraceCtrl1sub{ss},'r-','LineWidth',2);
+    ps1 = plot(timeDim,BtraceCtrl1sub{ss},'c-','LineWidth',2);
     ps1.Color(4) = 0.25;
-    ps2 = plot(timeDim,BtraceCtrl2sub{ss},'r--','LineWidth',2);
+    ps2 = plot(timeDim,BtraceCtrl2sub{ss},'m-','LineWidth',2);
     ps2.Color(4) = 0.25;
 end
 for ss = 1:length(BtraceOT1sub)
     ps1 = plot(timeDim,BtraceOT1sub{ss},'b-','LineWidth',2);
     ps1.Color(4) = 0.25;
-    ps2 = plot(timeDim,BtraceOT2sub{ss},'b--','LineWidth',2);
+    ps2 = plot(timeDim,BtraceOT2sub{ss},'r-','LineWidth',2);
     ps2.Color(4) = 0.25;
 end
 p1 = plot(timeDim,squeeze(BtraceOT1),'b-','LineWidth',4);
-p2 = plot(timeDim,squeeze(BtraceOT2),'b:','LineWidth',4);
-p3 = plot(timeDim,squeeze(BtraceCtrl1),'r-','LineWidth',4);
-p4 = plot(timeDim,squeeze(BtraceCtrl2),'r:','LineWidth',4);
+p2 = plot(timeDim,squeeze(BtraceOT2),'r-','LineWidth',4);
+p3 = plot(timeDim,squeeze(BtraceCtrl1),'c-','LineWidth',4);
+p4 = plot(timeDim,squeeze(BtraceCtrl2),'m-','LineWidth',4);
 axis([-.5, 2.5, -0.6, 0.4])
 line([0 0],[-0.6, 0.4],'color',[0.5 .5 .5],'LineStyle','--','LineWidth',2)
 xlabel('Time (s)','fontsize',16);
@@ -179,15 +84,15 @@ legend([p1,p2,p3,p4],{'OT foot','OT hand','HC foot','HC hand'},'Location','South
 legend BOXOFF
 
 % Save
-export_fig('beta_evo.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
+%export_fig('beta_evo.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
 % % close
 
-%% #############Single subject plots for OT and HC groups ###################
+%% Single subject plots
 
 fig = figure('rend','painters','pos',[10 10 750 1250]); hold on
 set(fig,'PaperPosition', [0 0 4 2], 'color','w');
 for ss = 1:length(OT_beta1bs)
-    subplot(4,3,ss); hold on
+    subplot(4,5,ss); hold on
     axis([-.5, 2.5, -0.6, 0.4])
     line([0 0],[-0.6, 0.5],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
     line([-.5, 2.5],[0 0],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
@@ -207,12 +112,12 @@ for ss = 1:length(OT_beta1bs)
     title(['OT ',num2str(ss)],'fontsize',12);
     set(gca, 'LineWidth', 2,'fontweight','bold');
 end
-export_fig('beta_evo_subOT2.png', '-r500', '-q100','-p0.05', '-CMYK', '-png', '-transparent')
+%export_fig('beta_evo_subOT2.png', '-r500', '-q100','-p0.05', '-CMYK', '-png', '-transparent')
 
 fig = figure('rend','painters','pos',[10 10 1000 1250]); hold on
 set(fig,'PaperPosition', [0 0 4 2], 'color','w');
 for ss = 1:length(ctrl_beta1bs)
-    subplot(4,4,ss); hold on
+    subplot(4,5,ss); hold on
     axis([-.5, 2.5, -0.6, 0.4])
     line([0 0],[-0.6, 0.5],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
     line([-.5, 2.5],[0 0],'color',[0.1 0.1 0.1],'LineStyle',':','LineWidth',1)
@@ -232,22 +137,3 @@ for ss = 1:length(ctrl_beta1bs)
     title(['HC ',num2str(ss)],'fontsize',12);
     set(gca, 'LineWidth', 2,'fontweight','bold');
 end
-%export_fig('beta_evo_subHC2.png', '-r500', '-q100','-p0.05', '-CMYK', '-png', '-transparent')
-
-% Extra plot
-fig = figure('rend','painters','pos',[10 10 600 400]); hold on
-set(fig,'PaperPosition', [0 0 4 2], 'color','w');
-patch([clustStart clustEnd clustEnd clustStart],[-0.6 -0.6 0.4 0.4],[.1,.1,.1],'FaceAlpha',0.1,'EdgeColor','none')
-p1=plot(BdataCtrl1.time,BtraceCtrl1,'-','LineWidth',3, 'color',[.5,.5,.5]);
-p2=plot(BdataCtrl2.time,BtraceCtrl2,':','LineWidth',3, 'color',[.5,.5,.5]);
-p3=plot(BdataCtrl1.time,BtraceOT1,'-','LineWidth',3, 'color',[.1,.1,.1]);
-p4=plot(BdataCtrl2.time,BtraceOT2,'k:','LineWidth',3, 'color',[.1,.1,.1]);    
-axis([-.5, 2.5, -0.4, 0.2])
-line([0 0],[-0.6, 0.4],'color',[0.5 .5 .5],'LineStyle','--','LineWidth',2)
-xlabel('Time (s)','fontsize',16);
-title('Beta-band spectral evolution','fontsize',16);
-set(gca, 'LineWidth', 2,'fontweight','bold','fontsize',14);
-legend([p1,p2,p3,p4],{'OT foot','OT finger','HC foot','HC finger'},'Location','SouthEast')
-legend BOXOFF
-export_fig('beta_evoBW.png', '-r500', '-p0.05', '-CMYK', '-png', '-transparent')
-
